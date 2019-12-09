711682fc3a604ee1105672d4aac306965dc88f5e
hernanspc <iphone1198@icloud.com>
2019-12-09 09:43:33 -0500

    first commit

---
+++ src/main/java/com/springwebflux/bootcamp/service/bussines/app/SpringwebfluxBootcampServiceBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.bussines.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxBootcampServiceBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxBootcampServiceBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springwebflux/bootcamp/service/bussines/app/SpringwebfluxBootcampServiceBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.bussines.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxBootcampServiceBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
