2d7e5f854124d8238a4274e885adb9b83f132e7e
Hernan <iphone1198@icloud.com>
2020-01-06 09:36:54 -0500

    add

--- src/main/java/com/springwebflux/bootcamp/service/business/app/model/Business.java
+++ src/main/java/com/springwebflux/bootcamp/service/business/app/model/Business.java
@@ -1,17 +1,36 @@
 package com.springwebflux.bootcamp.service.business.app.model;
+import java.util.Date;
+
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 @Document(collection="business")
 public class Business {
 	
-@Id
-	private String id;
-	private String nrodoc;
-	private String name;
-	private String mail;
-	private String phone;
-	private String status;
-	private String client_type;
+	  @Id
+	  private String id;
+
+	  @NotEmpty
+	  private String name;
+
+	  @NotEmpty
+	  private String ruc;
+
+	  @NotEmpty
+	  private String representLegal;
+
+	  @NotEmpty
+	  private String typeDoc;
+
+	  @NotEmpty
+	  private String numDoc;
+
+	  @NotEmpty
+	  @JsonFormat(pattern = "dd-MM-yyyy")
+	  private Date createdAt;
 	
 	 
 	
--- src/main/java/com/springwebflux/bootcamp/service/bussines/app/SpringwebfluxBootcampServiceBusinessApplication.java
+++ src/main/java/com/springwebflux/bootcamp/service/bussines/app/SpringwebfluxBootcampServiceBusinessApplication.java
@@ -1,6 +1,8 @@
 package com.springwebflux.bootcamp.service.bussines.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringwebfluxBootcampServiceBusinessApplication {
 	public static void main(String[] args) {
